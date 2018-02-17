a422f3f0cc1022c020d80699b0fa5bac83d23a58
Faris <faris@go-jek.com>
2018-02-17 14:15:19 +0700

    Updated travis-ci config file. Disabled before_install. Temporary try new install dep method.

---
+++ install-dep.sh
@@ -0,0 +1,11 @@
+go get -u -v gopkg.in/yaml.v2 &&
+go get -u -v gopkg.in/mgo.v2 &&
+go get -u -v github.com/jinzhu/gorm &&
+go get -u -v github.com/jinzhu/gorm/dialects/postgres &&
+go get -u -v github.com/jinzhu/gorm/dialects/mysql &&
+go get -u -v github.com/stretchr/testify &&
+go get -u -v github.com/stretchr/testify/assert &&
+go get -u -v github.com/jinzhu/inflection &&
+go get -u -v github.com/go-sql-driver/mysql &&
+go get -u -v github.com/lib/pq &&
+go get -u -v github.com/lib/pq/hstore
